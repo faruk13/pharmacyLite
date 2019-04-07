23f2307240801402f808ccec464595cfe77bfdd5
Umar Faruk <faruk13@users.noreply.github.com>
2019-04-07 12:14:55 +0530

    removed unrelated columns from class

--- forms.py
+++ forms.py
@@ -10,7 +10,6 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Sign In for Admin')
 
 class AddToMedStock(FlaskForm):
-    #medId=IntegerField('Med Id', validators=[DataRequired()]) 
     medName=StringField('MedName', validators=[DataRequired()])
     toManufId=StringField('Manufacturer Id', validators=[DataRequired()])
     wantQnty=IntegerField('Order Quantity', validators=[DataRequired()])
@@ -22,5 +21,4 @@ class CustomerOrder(FlaskForm):
     cusName=StringField('Customer Name', validators=[DataRequired()])
     medId=IntegerField('Med Id', validators=[DataRequired()])
     buyQnty=IntegerField('Buy Quantity', validators=[DataRequired()])
-    #CO1=IntegerField('Price', validators=[DataRequired()])
     submit = SubmitField('Buy')
